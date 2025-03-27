e2fecdd4735608ad791082afe47742611ab7d4e2
ben-lawino <lawinwes@gmail.com>
2025-03-27 13:47:04 +0300

    worked on the homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,11 @@ import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
 }
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    label: Text('Amount'),
+                    prefixText: '\$ ',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: () {}, child: Text('Cancel')),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.toString());
+                },
+                child: Text('Save Expense'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
